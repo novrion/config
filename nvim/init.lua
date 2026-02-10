287cf41c616471993e121c569cb332d40320312f
novrion <elindstenz@outlook.com>
2026-02-10 18:46:44 +0100

    feat: gemini 3 flash for awb

--- nvim/init.lua
+++ nvim/init.lua
@@ -53,7 +53,7 @@ vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 require("awb").setup({
-	model = "gemini-2.5-flash",
+	model = "gemini-3-flash-preview",
 	api_key = "",
 	keymaps = {
 		ask = { "<leader>a", { "n", "v" } }
